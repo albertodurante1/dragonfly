b1db60a8d1fc133e666c3b7f8f310a4e26e3e67f
alberto <albertodurante96@gmail.com>
2022-12-13 13:09:13 +0100

    adjustment

--- arduino/managerSensori.ino
+++ arduino/managerSensori.ino
@@ -7,22 +7,22 @@
 
 
 
-int pin1=14;
-int pin2=12;
+int pin1=13;
+int pin2=14;
 char jsonIP[1000];
 DynamicJsonDocument docIP(1024);
 JsonObject objIP=docIP.as<JsonObject>();
 
 String clientId;
 String stato[2]={"acceso","spento"};
-String topO[2] = {"movimento","topicdiff"};
+
   
 
  
- const char* ssid = "WIFIBETTO"; //inserire ssid della connessione
- const char* pass = "77777777";
+ const char* ssid = ""; //inserire ssid della connessione hotspot
+ const char* pass = "";
 
- const char* mqtt_server = "192.168.155.247"; //inserire indirizzo ip del server 127.0.0.1 non funziona
+ const char* mqtt_server = ""; //inserire indirizzo ip del server 127.0.0.1 non funziona
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -37,8 +37,8 @@ Topic led1("Led1",stato,2,"spento");
 Topic led2("Led2",stato,2,"spento");
 Topic mov("Movimento","non rilevato");
 Topic gas("Carbonio",String (analogRead(5)));
-Topic topicInput[20]={led1,led2};
-Topic topicOutput[20] = {gas};
+Topic topicInput[20]={led1};
+Topic topicOutput[20] = {mov};
 Device dispositivo("esp",topicInput,topicOutput);
 
  
@@ -76,41 +76,31 @@ void callback(char* topic, byte* payload, unsigned int length) {
     messaggio += (char)payload[i];
   }
   if(strcmp(topic,"MODIFICHE_SENSORI")==0){
-    //Serial.println("io so passato de qua");
     Serial.println(messaggio);
   }
   DynamicJsonDocument docMod(1024);
   Serial.print("Message arrived [");
   Serial.print(topic);
-  Serial.print("] ");
- 
-  
-  Serial.println();
-  
-  
+  Serial.print("] ");  
+  Serial.println();  
   deserializeJson(docMod, (char*) payload, length);
   String stateLed1 = docMod["topicListInput"][0]["stato"];
   String stateLed2 = docMod["topicListInput"][1]["stato"];
   if(stateLed1 == "acceso"){
     digitalWrite(pin1, HIGH);
-    Serial.println("led1 acceso");
-    delay(8000);
-    digitalWrite(pin1, LOW); 
-  }else{
+    Serial.println("led1 acceso");    
+    }else{
     digitalWrite(pin1, LOW); 
     Serial.println(" led1 spento"); 
-  } 
-  
-   if(stateLed2 == "acceso"){
+  }  
+
+  if(stateLed2 == "acceso"){
     digitalWrite(pin2, HIGH);
     Serial.println("led2 acceso");
   }else{
     digitalWrite(pin2, LOW); 
-    Serial.println(" led2 spento"); 
-  } 
-  
-  
-   
+    Serial.println("led2 spento"); 
+  }   
    
 }
 
@@ -124,12 +114,10 @@ void reconnect() {
     clientId += String(random(0xffff), HEX);
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");      
-      client.publish("DispositivoConnesso", "connesso");      
-      client.subscribe("MODIFICHE_SENSORI");
-      client.publish("newdevice", dispositivo.getJson("esp",topicInput,topicOutput));   
-      doc.clear();
-      client.publish("infoIP",jsonIP);
-      doc.clear();            
+      //client.publish("DispositivoConnesso", "connesso");      
+      client.subscribe("MODIFICHE_SENSORI"); 
+      String localIP = WiFi.localIP().toString().c_str();      
+      client.publish("newdevice", dispositivo.getJson("esp",localIP,topicInput,topicOutput));                       
       } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -154,12 +142,7 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  String localIP = WiFi.localIP().toString().c_str(); //importante metterlo qui dopo il setup del wifi altrimenti non lo vede
-  docIP["nome"]= "esp";
-  docIP["ip"] = localIP;
-  docIP["topic"] = topO[2];
-  serializeJson(docIP, jsonIP);
-  
+
   
   
   
@@ -169,7 +152,6 @@ void setup() {
 void loop() {
 
   
-  
  if (!client.connected()) {
     reconnect();
   }
@@ -177,12 +159,16 @@ void loop() {
 
 
 
-  //if(digitalRead(16) == HIGH)
-  //{   
-  //  client.publish("topicdiff", dispositivo.getJsonOutput("movimento","rilevato"));   
-   // doc.clear();
- // } // esempio di publish in caso di trigger dei sensori 
-        
+  if(digitalRead(16) == HIGH) // ascolto sul pin del sensore di movimento
+  {   
+    client.publish("readTopic", dispositivo.getJsonOutput("movimento","rilevato"));   
+   doc.clear();
+ }else{
+   delay(7000);
+   client.publish("readTopic", dispositivo.getJsonOutput("movimento","non rilevato")); 
+ } // esempio di publish in caso di trigger dei sensori 
+
+ // client.publish("outputtriggered", dispositivo.getJsonOutput("gas",String (analogRead(12))));   
    
      
   
